568d1fb21ca1c449d56992ae984b15b8a93fe7d4
Ajay Joseph <ajayj8843@gmail.com>
2025-03-11 17:09:06 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -49,14 +49,14 @@ function OffersGrid() {
     {
       title: "Dry fruits",
       discount: "Up to 65% Off",
-      brand: "T-shirts, Shirts, Jeans…",
-      image: "https://via.placeholder.com/150?text=UCB%2C+Arrow",
+      brand: "Dates, Almond, Hazelnuts…",
+      image: "https://img.freepik.com/premium-photo/dry-fruits-image-dark-wooden-surface_1036998-330106.jpg",
     },
     {
-      title: "Men's sports shoes",
-      discount: "Min. 50% Off",
-      brand: "New Balance & more",
-      image: "https://via.placeholder.com/150?text=Men%27s+Sports+Shoes",
+      title: "Appliances",
+      discount: "Max. 20% Off",
+      brand: "Refrigerator & more",
+      image: "https://www.brayandscarff.com/_plugins/site-pages/wordpress/wp-content/uploads/2017/07/BlackSS1.jpg",
     },
     {
       title: "Mosquito Bats & Lamps",
