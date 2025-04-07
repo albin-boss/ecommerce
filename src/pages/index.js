3b56c86f81caafb19f96a436cefc14558f579f9b
Ajay Joseph <ajayj8843@gmail.com>
2025-04-07 15:50:14 +0530

    4
    4

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,4 +8,4 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as Checkout } from './Checkout';
 export { default as PageNotFound } from './PageNotFound';
-export { default as Profilepage } from './Profile';
+
